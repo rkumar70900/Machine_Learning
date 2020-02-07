376e405163a52305f44df67f1568959ee9d6729f
rkumar70900 <54278302+rkumar70900@users.noreply.github.com>
2020-02-07 16:26:22 -0600

    Add files via upload

---
+++ Assignment_2/Assignment_2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  7 16:21:46 2020
+
+@author: rkuma
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+file = "C:\\Users\\rkuma\\OneDrive\\Documents\\Courses\\Semester 2\\Machine Learning\\My assignments\\Assignment 2\\Groceries.csv"
