6a6d054d3f1f23fa73028f092f26e0833cade45e
Rajesh Kumar Bandaru <rkumar70900@gmail.com>
2020-04-04 23:12:50 -0500

    question 1 complete

--- Assignment_4/Assignment_4.py
+++ Assignment_4/Assignment_4.py
@@ -72,12 +72,14 @@ def build_mnlogit (fullX, y, debug = 'N'):
 file = pd.read_csv('Purchase_Likelihood.csv',usecols=['group_size','homeowner','married_couple','insurance'])
 
 file = file.dropna()
+
 #target variable
 y = file['insurance'].astype('category')
 #predictor variables
 xG = pd.get_dummies(file[['group_size']].astype('category'))
 xH = pd.get_dummies(file[['homeowner']].astype('category'))
 xM = pd.get_dummies(file[['married_couple']].astype('category'))
+
 #Intercept model
 print('-'*50)
 print("Intercept model")
@@ -90,16 +92,10 @@ print('-'*50)
 print('Intercept + group_size')
 print('-'*50)
 designX = stats.add_constant(xG, prepend=True)
-LLK_1G, DF_1G, fullParams_1R = build_mnlogit (designX, y, debug = 'N')
+LLK_1G, DF_1G, fullParams_1G = build_mnlogit (designX, y, debug = 'N')
 testDev = 2 * (LLK_1G - LLK0)
 testDF = DF_1G - DF0
 testPValue = scipy.stats.chi2.sf(testDev, testDF)
-print('Number of Parameters = ',DF_1G)
-print('Log Likelihood = ',LLK_1G)
-print('Deviance = ', testDev)
-print('Degreee of Freedom = ', testDF)
-print('Significance = ', testPValue)
-print('Feature Importance Index = ',round(np.log10(testPValue),4))
 
 #Intercept + group_size + homeowner
 print('-'*50)
@@ -112,14 +108,7 @@ LLK_1G_1H, DF_1G_1H, fullParams_1G_1H = build_mnlogit (designX, y, debug = 'N')
 testDev = 2 * (LLK_1G_1H - LLK_1G)
 testDF = DF_1G_1H - DF_1G
 testPValue = scipy.stats.chi2.sf(testDev, testDF)
-print('Number of Parameters = ',DF_1G_1H)
-print('Log Likelihood = ',LLK_1G_1H)
-print('Deviance = ', testDev)
-print('Degreee of Freedom = ', testDF)
-print('Significance = ', testPValue)
-print('Feature Importance Index = ',round(np.log10(testPValue),4))
 
-#Intercept + group_size + homeowner + married_couple
 print('-'*50)
 print('Intercept +group_size + homeowner + married_couple')
 print('-'*50)
@@ -131,14 +120,7 @@ LLK_1G_1H_1M, DF_1G_1H_1M, fullParams_1G_1H_1M = build_mnlogit (designX, y, debu
 testDev = 2 * (LLK_1G_1H_1M - LLK_1G_1H )
 testDF = DF_1G_1H_1M - DF_1G_1H
 testPValue = scipy.stats.chi2.sf(testDev, testDF)
-print('Number of Parameters = ',DF_1G_1H_1M)
-print('Log Likelihood = ',LLK_1G_1H_1M)
-print('Deviance = ', testDev)
-print('Degreee of Freedom = ', testDF)
-print('Significance = ', testPValue)
-print('Feature Importance Index = ',round(np.log10(testPValue),4))
 
-#Intercept + group_size + homeowner + married_couple + group_size*homeowner
 print('-'*50)
 print('Intercept + group_size + homeowner + married_couple + group_size*homeowner')
 print('-'*50)
@@ -152,14 +134,7 @@ LLK_1G_1H_1M_GH, DF_1G_1H_1M_GH, fullParams_1G_1H_1M_GH = build_mnlogit (designX
 testDev = 2 * (LLK_1G_1H_1M_GH - LLK_1G_1H_1M )
 testDF = DF_1G_1H_1M_GH - DF_1G_1H_1M
 testPValue = scipy.stats.chi2.sf(testDev, testDF)
-print('Number of Parameters = ',DF_1G_1H_1M_GH)
-print('Log Likelihood = ',LLK_1G_1H_1M_GH)
-print('Deviance = ', testDev)
-print('Degreee of Freedom = ', testDF)
-print('Significance = ', testPValue)
-print('Feature Importance Index = ',round(np.log10(testPValue),4))
 
-#Intercept + group_size + homeowner + married_couple + group_size*homeowner + group_size * married_couple
 print('-'*50)
 print('Intercept + group_size + homeowner + married_couple + group_size*homeowner + group_size * married_couple')
 print('-'*50)
@@ -168,6 +143,7 @@ designX = designX.join(xH)
 designX = designX.join(xM)
 xGH = create_interaction(xG, xH)
 designX = designX.join(xGH)
+designX = stats.add_constant(designX, prepend=True)
 xGM = create_interaction(xG, xM)
 designX = designX.join(xGM)
 designX = stats.add_constant(designX, prepend=True)
@@ -175,14 +151,7 @@ LLK_1G_1H_1M_GH_GM, DF_1G_1H_1M_GH_GM, fullParams_1G_1H_1M_GH_GM = build_mnlogit
 testDev = 2 * (LLK_1G_1H_1M_GH_GM - LLK_1G_1H_1M_GH )
 testDF = DF_1G_1H_1M_GH_GM - DF_1G_1H_1M_GH
 testPValue = scipy.stats.chi2.sf(testDev, testDF)
-print('Number of Parameters = ',DF_1G_1H_1M_GH_GM)
-print('Log Likelihood = ',LLK_1G_1H_1M_GH_GM)
-print('Deviance = ', testDev)
-print('Degreee of Freedom = ', testDF)
-print('Significance = ', testPValue)
-print('Feature Importance Index = ',round(np.log10(testPValue),4))
 
-#Intercept + group_size + homeowner + married_couple + group_size*homeowner + group_size * married_couple + homeowner * married_couple 
 print('-'*50)
 print('Intercept + group_size + homeowner + married_couple + group_size*homeowner + group_size * married_couple + homeowner * married_couple' )
 print('-'*50)
@@ -191,25 +160,204 @@ designX = designX.join(xH)
 designX = designX.join(xM)
 xGH = create_interaction(xG, xH)
 designX = designX.join(xGH)
+designX = stats.add_constant(designX, prepend=True)
 xGM = create_interaction(xG, xM)
 designX = designX.join(xGM)
+designX = stats.add_constant(designX, prepend=True)
 xHM = create_interaction(xH, xM)
 designX = designX.join(xHM)
 designX = stats.add_constant(designX, prepend=True)
-LLK_1G_1H_1M_GH_GM_HM, DF_1G_1H_1M_GH_GM_HM, fullParams_1G_1H_1M_GH_GM_HM= build_mnlogit (designX, y, debug = 'N')
+LLK_1G_1H_1M_GH_GM_HM, DF_1G_1H_1M_GH_GM_HM, fullParams_1G_1H_1M_GH_GM_HM= build_mnlogit (designX, y, debug = 'Y')
 testDev = 2 * (LLK_1G_1H_1M_GH_GM_HM - LLK_1G_1H_1M_GH_GM )
 testDF = DF_1G_1H_1M_GH_GM_HM - DF_1G_1H_1M_GH_GM
 testPValue = scipy.stats.chi2.sf(testDev, testDF)
-print('Number of Parameters = ',DF_1G_1H_1M_GH_GM_HM)
-print('Log Likelihood = ',LLK_1G_1H_1M_GH_GM_HM)
+#question 1.b
+print('-'*50)
+print("question 1.b")
+print('-'*50)
+print('Degreee of Freedom = ', testDF)
+
+#question 1.c
+print('-'*50)
+print('question 1.c and question 1.d')
+print('-'*50)
+print('-'*50)
+print("Intercept model")
+print('-'*50)
+designX = pd.DataFrame(y.where(y.isnull(), 1))
+LLK0, DF0, fullParams0 = build_mnlogit (designX, y, debug = 'Y')
+print('Log Likelihood = ',LLK0)
+print('Degree of Freedom = ',DF0)
+
+print('-'*50)
+print('Intercept + group_size')
+print('-'*50)
+designX = stats.add_constant(xG, prepend=True)
+LLK_G, DF_G, fullParams_G = build_mnlogit (designX, y, debug = 'N')
+testDev = 2 * (LLK_G - LLK0)
+testDF = DF_G - DF0
+testPValue = scipy.stats.chi2.sf(testDev, testDF)
+print('Number of Parameters = ',DF_G)
+print('Log Likelihood = ',LLK_G)
 print('Deviance = ', testDev)
 print('Degreee of Freedom = ', testDF)
 print('Significance = ', testPValue)
-print('Feature Importance Index = ',round(np.log10(testPValue),4))
+print("Feature Importance Index = ",round(-np.log10(testPValue)),4)
 
+#intercept + homeowner
+print('-'*50)
+print('Intercept + homeowner')
+print('-'*50)
+designX = stats.add_constant(xH, prepend=True)
+LLK_H, DF_H, fullParams_H = build_mnlogit (designX, y, debug = 'N')
+testDev = 2 * (LLK_H - LLK0)
+testDF = DF_H - DF0
+testPValue = scipy.stats.chi2.sf(testDev, testDF)
+print('Number of Parameters = ',DF_H)
+print('Log Likelihood = ',LLK_H)
+print('Deviance = ', testDev)
+print('Degreee of Freedom = ', testDF)
+print('Significance = ', testPValue)
+print("Feature Importance Index = ",round(-np.log10(testPValue)),4)
 
-reduced_form, inds = sympy.Matrix(designX.values).rref()
+#intercept + marriedcouple
+print('-'*50)
+print('Intercept + marriedcouple')
+print('-'*50)
+designX = stats.add_constant(xM, prepend=True)
+LLK_M, DF_M, fullParams_M = build_mnlogit (designX, y, debug = 'N')
+testDev = 2 * (LLK_M - LLK0)
+testDF = DF_M - DF0
+testPValue = scipy.stats.chi2.sf(testDev, testDF)
+print('Number of Parameters = ',DF_M)
+print('Log Likelihood = ',LLK_M)
+print('Deviance = ', testDev)
+print('Degreee of Freedom = ', testDF)
+print('Significance = ', testPValue)
+print("Feature Importance Index = ",round(-np.log10(testPValue)),4)
 
+#intercept + groupsize + homeowner
+print('-'*50)
+print('Intercept + groupsize + homeowner')
+print('-'*50)
+designX = xG
+designX = designX.join(xH)
+designX = stats.add_constant(designX, prepend=True)
+LLK_GH, DF_GH, fullParams_GH = build_mnlogit (designX, y, debug = 'N')
+testDev = 2 * (LLK_GH - LLK_G)
+testDF = DF_GH - DF_G
+testPValue = scipy.stats.chi2.sf(testDev, testDF)
+print('Number of Parameters = ',DF_GH)
+print('Log Likelihood = ',LLK_GH)
+print('Deviance = ', testDev)
+print('Degreee of Freedom = ', testDF)
+print('Significance = ', testPValue)
+print("Feature Importance Index = ",round(-np.log10(testPValue)),4)
+
+#intercept + groupsize + marriedcouple
+print('-'*50)
+print('Intercept + groupsize + marriedcouple')
+print('-'*50)
+designX = xG
+designX = designX.join(xM)
+designX = stats.add_constant(designX, prepend=True)
+LLK_GM, DF_GM, fullParams_GM = build_mnlogit (designX, y, debug = 'N')
+testDev = 2 * (LLK_GM - LLK_G)
+testDF = DF_GM - DF_G
+testPValue = scipy.stats.chi2.sf(testDev, testDF)
+print('Number of Parameters = ',DF_GM)
+print('Log Likelihood = ',LLK_GM)
+print('Deviance = ', testDev)
+print('Degreee of Freedom = ', testDF)
+print('Significance = ', testPValue)
+print("Feature Importance Index = ",round(-np.log10(testPValue)),4)
+
+#intercept + homeowner + marriedcouple
+print('-'*50)
+print('Intercept + homeowner + marriedcouple')
+print('-'*50)
+designX = xH
+designX = designX.join(xM)
+designX = stats.add_constant(designX, prepend=True)
+LLK_HM, DF_HM, fullParams_HM = build_mnlogit (designX, y, debug = 'N')
+testDev = 2 * (LLK_HM - LLK_H)
+testDF = DF_HM - DF_H
+testPValue = scipy.stats.chi2.sf(testDev, testDF)
+print('Number of Parameters = ',DF_HM)
+print('Log Likelihood = ',LLK_HM)
+print('Deviance = ', testDev)
+print('Degreee of Freedom = ', testDF)
+print('Significance = ', testPValue)
+print("Feature Importance Index = ",round(-np.log10(testPValue)),4)
+
+#question 2.a
+print('The Model')
+print('-'*50)
+print('Intercept + group_size + homeowner + married_couple + group_size*homeowner + group_size * married_couple + homeowner * married_couple' )
+print('-'*50)
+designX = xG
+designX = designX.join(xH)
+designX = designX.join(xM)
+xGH = create_interaction(xG, xH)
+designX = designX.join(xGH)
+designX = stats.add_constant(designX,prepend = True)
+xGM = create_interaction(xG, xM)
+designX = designX.join(xGM)
+designX = stats.add_constant(designX,prepend = True)
+xHM = create_interaction(xH, xM)
+designX = designX.join(xHM)
+designX = stats.add_constant(designX, prepend=True)
+y = file['insurance'].astype('category')
+
+#Training the model
+final_model = stats.MNLogit(y,designX)
+final_model_fit = final_model.fit(method='newton', full_output=True, maxiter=100, tol=1e-8)
+
+#Getting all the possibilities
+GS_P = [1, 2, 3, 4]
+HO_P = [0, 1]
+MC_P = [0, 1]
+
+all_pos = []
+
+for i in GS_P:
+    for j in HO_P:
+        for k in MC_P:
+            pos = [i,j,k]
+            all_pos = all_pos + [pos]
+
+#All possibilities as DataFrame
+inp = pd.DataFrame(all_pos,columns=['group_size','homeowner','married_couple']) 
+xG = pd.get_dummies(inp[['group_size']].astype('category'))
+xH = pd.get_dummies(inp[['homeowner']].astype('category'))
+xM = pd.get_dummies(inp[['married_couple']].astype('category'))
+designX = xG
+designX = designX.join(xH)
+designX = designX.join(xM)
+xGH = create_interaction(xG, xH)
+designX = designX.join(xGH)
+designX = stats.add_constant(designX,prepend = True)
+xGM = create_interaction(xG, xM)
+designX = designX.join(xGM)
+designX = stats.add_constant(designX,prepend = True)
+xHM = create_interaction(xH, xM)
+designX = designX.join(xHM)
+designX = stats.add_constant(designX, prepend=True)
+        
+#predicting for the possibilities of group_size, homeowner, married_couple
+pred = final_model_fit.predict(exog=designX)
+pred['insurance_0'] = pred[0]
+pred['insurance_1'] = pred[1]
+pred['insurance_2'] = pred[2]
+
+out = pd.concat([inp,pred[['insurance_0','insurance_1','insurance_2']]],axis=1)
+
+print('-'*50)
+print('question 2.a')
+print('-'*50)
+print(out)
+
+    
 
 
 
