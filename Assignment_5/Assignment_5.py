03c86d0980cef2b1bb1b6efc6ed539fe5786f5d2
Rajesh Kumar Bandaru <rkumar70900@gmail.com>
2020-04-22 16:57:39 -0500

    question 1

--- Assignment_5/Assignment_5.py
+++ Assignment_5/Assignment_5.py
@@ -3,6 +3,8 @@ import sklearn.neural_network as nn
 import sklearn.metrics as metrics
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
+import statistics as st
 
 spiral = pd.read_csv('SpiralWithCluster.csv')
 
@@ -30,12 +32,11 @@ def Build_NN_Toy (nLayer, nHiddenNeuron,act_func):
     y_pred = nnObj.predict_proba(spiral[['x','y']])
 
     Loss = nnObj.loss_
-    #RSquare = 2
+    iter = nnObj.n_iter_
 
     y_t = spiral['SpectralCluster']
 
     pred = []
-
     for i in range(len(y_t)):
         if(y_pred[i][0]>=threshold):
             pred.append(0)
@@ -44,39 +45,78 @@ def Build_NN_Toy (nLayer, nHiddenNeuron,act_func):
 
     accuracy = metrics.accuracy_score(y_t,pred)
     missclassification_rate = 1-accuracy
+    return (Loss,missclassification_rate,act_func,iter)
 
-
-
-    return (Loss,missclassification_rate,act_func)
-
-result = pd.DataFrame(columns = ['nLayer', 'nHiddenNeuron', 'Loss', 'RSquare','Activation Function'])
+print('question 1.b')
+result = pd.DataFrame(columns = ['Activation Function','nLayer', 'nHiddenNeuron','Iterations','Loss', 'MissClassification Rate'])
+#result2 = pd.DataFrame(columns = ['Activation Function','nLayer', 'nHiddenNeuron','Iterations','Loss', 'MissClassification Rate'])
 act_function = ['relu','identity','logistic','tanh']
-
-
 
 for k in act_function:
     min_loss = float('inf')
     min_class = float('inf')
     for i in np.arange(1,6):
         for j in np.arange(1,11,1):
-                Loss, RSquare, act_func = Build_NN_Toy (nLayer = i, nHiddenNeuron = j,act_func=k)
-                if(Loss<min_loss and RSquare<min_class):
-                    min_loss=Loss
-                    min_class = RSquare
-                    layer = i
-                    hid = j
-                    act_func = k
-    result = result.append(pd.DataFrame([[layer,hid,min_loss,min_class,act_func]],
-                                       columns = ['nLayer', 'nHiddenNeuron', 'Loss', 'RSquare','Activation Function']))
-
+            Loss, missclass, act_func,iter = Build_NN_Toy(nLayer = i, nHiddenNeuron = j,act_func=k)
+            if(Loss<=min_loss and missclass<=min_class):
+                min_loss = Loss
+                min_class = missclass
+                layer = i
+                hid = j
+                it = iter
+                act_func = k
+            #result2 = result2.append(pd.DataFrame([[act_func,i,j,iter,Loss,missclass]],columns=['Activation Function','nLayer', 'nHiddenNeuron','Iterations','Loss', 'MissClassification Rate']))
+    result = result.append(pd.DataFrame([[act_func,layer,hid,it,min_loss,min_class]],
+                                       columns =['Activation Function','nLayer', 'nHiddenNeuron','Iterations','Loss', 'MissClassification Rate']))
 
 
 pd.set_option('display.max_rows', result.shape[0]+1)
 pd.set_option('display.max_columns', result.shape[1]+1)
-#result = result.groupby('Activation Function')['Loss'].min()
+#print(result2)
+result.nLayer = result.nLayer.astype('int64')
+result.nHiddenNeuron = result.nHiddenNeuron.astype('int64')
+result.Iterations = result.Iterations.astype('int64')
+
+result.reset_index(drop=True,inplace=True)
 print(result)
 
-g = pd.pivot_table(result,index=["Activation Function"],values=['Loss','RSquare'],aggfunc={'Loss':np.min,'RSquare':np.min})
-pd.set_option('display.max_rows', g.shape[0]+1)
-pd.set_option('display.max_columns', g.shape[1]+1)
-#print(g)
+index = result['Loss'].idxmin()
+print('question 1.d')
+pd.set_option('display.max_rows', 10)
+print(result.iloc[index,:])
+print('question 1.e')
+min_hidden = int(result.iloc[index,2])
+min_layers = int(result.iloc[index,1])
+nnObj = nn.MLPClassifier(hidden_layer_sizes = (min_hidden,)*min_layers,
+                        activation = 'relu', verbose = False,
+                        solver = 'lbfgs', learning_rate_init = 0.1,
+                        max_iter = 5000, random_state = 20200408)
+
+thisFit = nnObj.fit(spiral[['x','y']], spiral['SpectralCluster'])
+y_pred = nnObj.predict_proba(spiral[['x','y']])
+
+y_t = spiral['SpectralCluster']
+pred = []
+for i in range(len(y_t)):
+    if(y_pred[i][0]>=threshold):
+        pred.append(0)
+    else:
+        pred.append(1)
+spiral['Predicted'] = pred
+
+colors = ['red', 'blue']
+for i in range(2):
+    i_data = spiral[spiral['Predicted'] == i]
+    plt.scatter(x = i_data['x'],y=i_data['y'],c = colors[i], label = i, s = 25,linestyle='-')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.show()
+print('question 1.c')
+print("Activation Function of output function= {}".format(nnObj.out_activation_))
+print('question 1.f')
+
+pred_1 = [y_pred[i][1] for i in range(len(y_pred)) if y_pred[i][1]>=0.5]
+print(f"Count :{len(pred_1):.10f}")
+print(f"Mean: {sum(pred_1)/len(pred_1):.10f}")
+print(f"Standard Deviation: {st.stdev(pred_1):.10f}")
